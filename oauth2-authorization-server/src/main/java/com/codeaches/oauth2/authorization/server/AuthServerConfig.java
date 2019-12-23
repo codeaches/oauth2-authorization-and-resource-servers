37747487717df09af5b5e1b32692e2c9b778c5ba
codeaches <info001@codeaches.com>
2019-12-23 09:33:44 -0700

    JDBC OAuth2 Resource Server Example [http://codeaches.com/]

--- oauth2-authorization-server/src/main/java/com/codeaches/oauth2/authorization/server/AuthServerConfig.java
+++ oauth2-authorization-server/src/main/java/com/codeaches/oauth2/authorization/server/AuthServerConfig.java
@@ -21,45 +21,45 @@ import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    DataSource ds;
+  @Autowired
+  DataSource ds;
 
-    @Autowired
-    AuthenticationManager authMgr;
+  @Autowired
+  AuthenticationManager authMgr;
 
-    @Autowired
-    private UserDetailsService usrSvc;
+  @Autowired
+  private UserDetailsService usrSvc;
 
-    @Bean
-    public TokenStore tokenStore() {
-	return new JdbcTokenStore(ds);
-    }
+  @Bean
+  public TokenStore tokenStore() {
+    return new JdbcTokenStore(ds);
+  }
 
-    @Bean("clientPasswordEncoder")
-    PasswordEncoder clientPasswordEncoder() {
-	return new BCryptPasswordEncoder(8);
-    }
+  @Bean("clientPasswordEncoder")
+  PasswordEncoder clientPasswordEncoder() {
+    return new BCryptPasswordEncoder(8);
+  }
 
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer cfg) throws Exception {
+  @Override
+  public void configure(AuthorizationServerSecurityConfigurer cfg) throws Exception {
 
-	// This will enable /oauth/check_token access
-	cfg.checkTokenAccess("permitAll");
+    // This will enable /oauth/check_token access
+    cfg.checkTokenAccess("permitAll");
 
-	// BCryptPasswordEncoder(8) is used for oauth_client_details.user_secret
-	cfg.passwordEncoder(clientPasswordEncoder());
-    }
+    // BCryptPasswordEncoder(8) is used for oauth_client_details.user_secret
+    cfg.passwordEncoder(clientPasswordEncoder());
+  }
 
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-	clients.jdbc(ds);
-    }
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients.jdbc(ds);
+  }
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 
-	endpoints.tokenStore(tokenStore());
-	endpoints.authenticationManager(authMgr);
-	endpoints.userDetailsService(usrSvc);
-    }
+    endpoints.tokenStore(tokenStore());
+    endpoints.authenticationManager(authMgr);
+    endpoints.userDetailsService(usrSvc);
+  }
 }
--- oauth2-authorization-server/src/main/java/com/codeaches/oauth2/authorization/server/Oauth2AuthorizationServerApplication.java
+++ oauth2-authorization-server/src/main/java/com/codeaches/oauth2/authorization/server/Oauth2AuthorizationServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Oauth2AuthorizationServerApplication {
 
-    public static void main(String[] args) {
-	SpringApplication.run(Oauth2AuthorizationServerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Oauth2AuthorizationServerApplication.class, args);
+  }
 }
