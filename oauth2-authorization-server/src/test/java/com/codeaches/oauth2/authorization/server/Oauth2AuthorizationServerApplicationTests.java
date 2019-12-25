6351b00c62d0f3a8622acb0b5fc954567198866b
codeaches <info001@codeaches.com>
2019-12-25 06:43:31 -0700

    JDBC OAuth2 Resource Server Example [http://codeaches.com/]

--- oauth2-authorization-server/src/test/java/com/codeaches/oauth2/authorization/server/Oauth2AuthorizationServerApplicationTests.java
+++ oauth2-authorization-server/src/test/java/com/codeaches/oauth2/authorization/server/Oauth2AuthorizationServerApplicationTests.java
@@ -1,25 +1,12 @@
 package com.codeaches.oauth2.authorization.server;
 
-import java.util.Base64;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Oauth2AuthorizationServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
   @Test
-  void generateBase64EncodedValue() {
-
-    // Get the Base64 password for appclient:appclient@123
-    String base64AuthHeader = Base64.getEncoder().encodeToString("appclient:appclient@123".getBytes());
-
-    // This Base64 password for appclient:appclient@123 will be used in the http
-    // header when requesting the token
-    System.out.println("appclient's Base64 Password is " + base64AuthHeader);
+  void contextLoads() {
   }
 }
