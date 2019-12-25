296221e60f8d53051925650b11cc68aded1623aa
codeaches <info001@codeaches.com>
2019-12-25 07:43:55 -0700

    JDBC OAuth2 Resource Server Example [http://codeaches.com/]

--- oauth2-authorization-server/src/main/java/com/codeaches/oauth2/authorization/server/Oauth2AuthorizationServerApplication.java
+++ oauth2-authorization-server/src/main/java/com/codeaches/oauth2/authorization/server/Oauth2AuthorizationServerApplication.java
@@ -2,7 +2,9 @@ package com.codeaches.oauth2.authorization.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@EnableAuthorizationServer
 @SpringBootApplication
 public class Oauth2AuthorizationServerApplication {
 
